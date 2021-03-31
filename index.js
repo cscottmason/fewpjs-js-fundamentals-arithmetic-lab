c44696ca3e6983c2c5ffa0418ab02fe53203d852
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-31 08:59:16 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random() * 10) + 1 ;
+
+
+
+let mod;
+let num3 = 10;
+let num4 = 6;
+
+mod = num3 % num4;
+
+let max;
+max = Math.max(2,4,6,8,10,12,14,16,18,20);
